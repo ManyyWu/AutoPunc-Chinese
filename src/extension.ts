fb720f782665ee90bcea3de64ffcd69f933a23cf
Manyy <manyy.openkt@gmail.com>
2026-03-25 15:28:36 +0800

    添加指定后缀的转换支持

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 'use strict';
+import * as path from 'path';
 import * as vscode from 'vscode';
 import { getPuncpairs } from './helpers';
 
@@ -8,11 +9,9 @@ correction = true;
 let myStatusBarItem: vscode.StatusBarItem;
 
 const hs = vscode.extensions.getExtension('draivin.hscopes');
-// console.log(hs);
 let hsAPI: any;
 hs?.activate().then(
   (ext) => {
-    // console.log(ext);
     hsAPI = ext;
   }
 );
@@ -34,30 +33,36 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
   myStatusBarItem.command = myCommandId;
   subscriptions.push(myStatusBarItem);
 
-//   try {
+  puncpairs = puncpairs || getPuncpairs();
+  vscode.workspace.onDidOpenTextDocument(() => {
     puncpairs = puncpairs || getPuncpairs();
-    vscode.workspace.onDidOpenTextDocument(() => {
-      puncpairs = puncpairs || getPuncpairs();
-    });
-    vscode.workspace.onDidChangeTextDocument(event => {
-      if (event.document.fileName === "git\\scm0\\input") { return; } // 解决 https://gitee.com/laowu2019_admin/autopunc/issues/I4SMS3
-      puncpairs = puncpairs || getPuncpairs();
-      if (correction) {
-        correctPunc(event);
-        // 把中文省略号 …… / —— 转换的两个 ^^ /__ 去掉一个
-        // correctDup(event);
-      }
-    });
-//   } catch (error) {
-//     console.log(error);      
-//   }
-  
-  // vscode.workspace.onDidChangeConfiguration(e => {
-  //   if (e.affectsConfiguration('auto-correct')) {
-  //     config = getConfig();
-  //   }
-  // });
+  });
+  vscode.workspace.onDidChangeTextDocument(event => {
+    if (event.document.fileName === "git\\scm0\\input") { return; }
+    puncpairs = puncpairs || getPuncpairs();
+    if (correction && isExtensionEnabled(event.document)) {
+      correctPunc(event);
+    }
+  });
 }
+
+function isExtensionEnabled(document: vscode.TextDocument): boolean {
+  const ext = path.extname(document.fileName).replace(/^\./, '');
+  if (!ext) {
+    return false;
+  }
+
+  const config = vscode.workspace.getConfiguration('autopunc', document.uri);
+  const extensions = config.get<{ [key: string]: boolean }>('extensions', {});
+
+  if (extensions && typeof extensions === 'object' && Object.prototype.hasOwnProperty.call(extensions, ext)) {
+    return Boolean(extensions[ext]);
+  }
+
+  // 默认启用，如果 autopunc.extensions 中未配置该后缀
+  return true;
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
@@ -68,7 +73,7 @@ function correctPunc(event: vscode.TextDocumentChangeEvent): void {
   }
 
   let lastChr = event.contentChanges[0].text;
-//   console.log(lastChr);
+  console.log(lastChr);
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
     return;
@@ -76,7 +81,7 @@ function correctPunc(event: vscode.TextDocumentChangeEvent): void {
 
   const keys = Object.keys(puncpairs);
 
-  if (keys.includes(lastChr) && lastChr !== puncpairs[lastChr]) { // #I4RIQ5；有了后一判断条件，用户自定义配置时可以取消某些中文标点的转换。
+  if (keys.includes(lastChr) && lastChr !== puncpairs[lastChr]) {
     editor.edit(
       editBuilder => {
         const contentChangeRange = event.contentChanges[0].range;
@@ -84,32 +89,19 @@ function correctPunc(event: vscode.TextDocumentChangeEvent): void {
         const startCharacter = contentChangeRange.start.character;
         let start: vscode.Position;
         let end: vscode.Position;
-        // if (lastChr === '…' || lastChr === '—') {
-        //   start = new vscode.Position(startLine, startCharacter - 1);
-        // } else {
-          start = new vscode.Position(startLine, startCharacter);
-        // }
+        start = new vscode.Position(startLine, startCharacter);
+
         if (lastChr === '……' || lastChr === '——') {
             end = new vscode.Position(startLine, startCharacter + 2);
         } else {
             end = new vscode.Position(startLine, startCharacter + 1);
         }
 
-        // 使用 vscode.proposed.tokenInformation.d.ts API 的代价太大，未进行 tokenization
-        // vscode.languages.getTokenInformationAtPosition(event.document, start).then(
-        //   (tokenInfo) => {
-        //     console.log(tokenInfo.range, tokenInfo.type);            
-        //     if (tokenInfo.type === vscode.StandardTokenType.Other) return;
-        //   }
-        // );
         if (hsAPI) {
           const token = hsAPI.getScopeAt(event.document, start);
-          // console.log(token);
-          // console.log(token.scopes);
           if (token) {
             for (let item of token.scopes) {
-              // if (item.match(/comment|string/) && !item.match(/regex/)) throw new Error("注释或字符串中不转换");
-              if (item.match(/comment/) && !item.match(/json/)) throw new Error("注释中不转换"); // json 文件的 scope 都带 comment？
+              if (item.match(/comment/) && !item.match(/json/)) throw new Error("注释中不转换");
             }
           }
         }
@@ -123,51 +115,7 @@ function correctPunc(event: vscode.TextDocumentChangeEvent): void {
       }
     ).then(
       () => {
-        // vscode.commands.executeCommand("type", { text: puncpairs[lastChr] }).then(() => {
-          // 删除多余的字符
-        //   const change = event.contentChanges[0];
-        //   const deletePosition = new vscode.Position(change.range.start.line, change.range.start.character + 1);
-        //   editor.edit(editBuilder => {
-        //     editBuilder.delete(new vscode.Range(deletePosition, deletePosition.translate(0, 1)));
-        //   });
-        // });
       }
     );    
   }
 }
-
-// function correctDup(event: vscode.TextDocumentChangeEvent): void {
-//   if (!event.contentChanges.length) {
-//     return;
-//   }
-
-//   const editor = vscode.window.activeTextEditor;
-//   if (!editor) {
-//     return;
-//   }
-
-//   const contentChangeRange = event.contentChanges[0].range;
-//   const startLine = contentChangeRange.start.line;
-//   const startCharacter = contentChangeRange.start.character;
-//   const start = new vscode.Position(startLine, Math.max(0, startCharacter - 1));
-//   const end = new vscode.Position(startLine, startCharacter + 1);
-//   const text = editor.document.getText(
-//     new vscode.Range(start, end)
-//   );
-//   // console.log(text);
-
-//   if (text === '^^'/*  || text === '__' */) { // https://gitee.com/laowu2019_admin/autopunc/issues/I4SMS3
-//     editor.edit(
-//       editBuilder => {
-//         const newStart = new vscode.Position(startLine, startCharacter);
-//         if (startCharacter) {
-//           editBuilder.delete(new vscode.Range(newStart, end));
-//         }
-//       },
-//       {
-//         undoStopAfter: false,
-//         undoStopBefore: false,
-//       }
-//     );    
-//   }
-// }
